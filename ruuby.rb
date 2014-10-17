026d7a43dc6a5a392d32dc87e34c1c474b04289e
pabloabarrio <pabloa.barriodiez@gmail.com>
2014-10-17 16:18:30 +0200

    Delete ruuby.rb

--- ruuby.rb
+++
@@ -1,6 +0,0 @@
-print "qué archivo quieres que coja?"
-saludo = gets.chomp
-contenido = IO.read(saludo)
-print "cómo se llama el nuevo archivo?"
-archivo = gets.chomp
-resultado = IO.write(archivo, contenido)
